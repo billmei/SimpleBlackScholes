4c3acccdf7c9ec74336083c142b0bd3829bb6033
Bill Mei <billmei@users.noreply.github.com>
2013-03-24 09:21:03 -0400

    Form reset

--- main.js
+++ main.js
@@ -2,3 +2,11 @@ $('#volatility').tooltip({
     'trigger':'focus',
     'placement':'right'
 });
+
+$('#reset').on('click', function () {
+    $('#mainForm')[0].reset();
+});
+
+$('#calculate').on('click', function() {
+
+});
