9f6a24761320ba23a6be2edf5597592445c2ea2f
Bill Mei <billmei@users.noreply.github.com>
2013-03-24 09:23:33 -0400

    quick fix

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ $('#volatility').tooltip({
     'placement':'right'
 });
 
+var k = $('#strike').val();
+var s = $('#priceunderlying').val();
+var T = $('#expiretime').val();
+var sigma = $('#volatility').val();
+var d = $('#dividend').val();
+var r = $('#riskfree').val();
+
 $('#reset').on('click', function () {
     $('#mainForm')[0].reset();
 });
